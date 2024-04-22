14a6b2b2492551208b2cfe9724dc775517c76758
SNSurajbhan <sn.surajbhan@gmail.com>
2024-04-22 15:49:56 +0530

    Update StringCalculator.Tests.cpp

--- StringCalculator.Tests.cpp
+++ StringCalculator.Tests.cpp
@@ -5,7 +5,7 @@ TEST(StringCalculatorTestSuite,GivenEmptyStringZeroIsExpected){
     //Arrange
     StringCalculator objUnderTest;
     string input="";
-    int expectedValue=4;
+    int expectedValue=0;
     //Act
    int actualValue=  objUnderTest.Add(input);
     //Assert
